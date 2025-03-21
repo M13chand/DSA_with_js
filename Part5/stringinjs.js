ba8986cd79b9fae075d0070ac6f451d3f60f9cd9
M13chand <mannuc359@gmail.com>
2025-03-21 23:26:33 +0545

    part5

---
+++ Part5/stringinjs.js
@@ -0,0 +1,6 @@
+let name = 'manoj';
+console.log(name);
+console.log(name[4]);
+
+let num = [10, 23, 4, 3, 44, 33, 47, 87];
+console.log(num[4])
---
+++ part4/arrays.js
@@ -0,0 +1,16 @@
+let arr = [];
+arr.push(10);
+arr.push(34);
+arr.push('manoj');
+arr.push(43);
+arr.pop(43);
+console.log(arr[2]); //print the value of the 3rd index of the array
+console.log(arr);
+
+let array = [];
+array[0] = 45;
+array[1] = 32;
+array[2] = 23;
+array[3] = 21
+
+console.log(array);

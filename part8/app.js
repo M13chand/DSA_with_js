608552f727c42931396eca71f218edfbd337a94e
M13chand <mannuc359@gmail.com>
2025-04-17 22:23:21 +0545

    bubble sort

---
+++ part8/app.js
@@ -0,0 +1,6 @@
+let prompt = require('prompt-sync')();
+let a = prompt("Enter a number");
+process.stdout.write(a);
+
+
+process.stdout.write("Hello")
---
+++ part9/app.js
@@ -0,0 +1,15 @@
+// Bubble short  Algorithm
+let array = [10, 5, 12, 1, 3]
+let n = array.length;
+for (let i = 0; i < n - 1; i++) {
+  for (let j = 0; j < n - i - 1; j++) {
+    if (array[j] > array[j + 1]) {
+      let temp = array[j];
+      array[j] = array[j + 1];
+      array[j + 1] = temp;
+    }
+
+  }
+
+}
+console.log("The sorted array is ", array)
